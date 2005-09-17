4ff25f27b5387b254dbb62ae6c75990550dd9997
fjmd1 <fjmd1@9cc54934-f9f6-0310-8a8d-e8e977684441>
2005-09-17 16:49:39 +0000

    analyses an the local government finance act 1988 (1988c41) up to the Schedules
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@219 9cc54934-f9f6-0310-8a8d-e8e977684441

--- lawparse/opsi/analyser.py
+++ lawparse/opsi/analyser.py
@@ -1,6 +1,8 @@
 
 import re
 import sys
+from parsefun import *
+
 
 actname='acts/1988/41.html'
 actfile=open(actname,'r')
@@ -10,7 +12,6 @@ url=re.search('<url>(.*?)</url>',act).group(1)
 chapno=re.search('<chapter_number>([1-9][0-9]*?)</chapter_number>',act).group(1)
 year=re.search('<year>([1-9][0-9]*?)</year>',act).group(1)
 
-
 def output(s,t, u=''):
 	t=t + ' act="%sc%s"' % (year,chapno)
 	if len(u)==0:
@@ -18,57 +19,15 @@ def output(s,t, u=''):
 	else:
 		print "<%s %s>%s</%s>" % (s,t,u,s)
 
-def path2opt(p): 	# converts a path into options suitable for xml
-	a=''
-	for x in range(0,len(p)):
-		a=a + (' %s="%s%s%s"' % ('s' * (x+1),p[x][2],p[x][0],p[x][3]))
-	return a
-
-def pathsearch(p,item): # searches a path for a matching item type
-	#print "***pathsearch entry point****",p,len(p),item
-	for x in range(0,len(p)):
-		if p[x][1]==item[1] and p[x][2]==item[2] and p[x][3]==item[3]:
-			#print "***pathsearch exit point 1***",p,item,x
-			return x
-
-	#print "***pathsearch exit point 2***",p,item,x
-	return len(p)
-
-def makeitem(s):
-	m=re.match('([1-9][0-9]*)',s)
-	if m:
-		return (m.group(1),'numeric','','')
-
-	m=re.match('\(([a-z])\)',s)
-	if m:
-		return (m.group(1),'lalpha','(',')')
-
-	m=re.match('\(([1-9][0-9]*)\)',s)
-	if m:
-		return (m.group(1),'numeric','(',')')
-
-	print "****Unknown enumeration item (%s)" % s
-
-def pathinsert(path,m):
-	item=makeitem(m.group(1))			
-	n=pathsearch(path,item)
-	if n==len(path):
-		path.append(item)
-	else:
-		path[n]=item
-		path=path[:n+1]			
-	#leafoptions=path2opt(path)+partoption
-	#print "****",n,path,item,m.group(1),right[:32]
-	
-
-
 
 def parseline(line):
 	path=[]
-	m=re.match('\s*<td width="20%">\s*<a name="mdiv(?:[1-9][0-9]*)"></a><br>\s*<font size="2">([ \w:.]*?)</font>\s*<br>\s*</td>\s*<td>((\s|\S)*?)</td>\s*</tr>',line)
+	# extract left and right parts of the line
+	m=re.match('\s*<td width="20%">\s*<a name="mdiv(?:[1-9][0-9]*)"></a><br>\s*<font size="2">([ \w\':\-.]*?)</font>\s*<br>\s*</td>\s*<td>((\s|\S)*?)</td>\s*$',line)
 	if m:
-		leftitem=m.group(1)
-		leftoption=' leftitem="%s"' % leftitem
+		marginalium=m.group(1)
+		print "****marginalium:",marginalium
+		leftoption=' margin="%s"' % marginalium
 	else:
 		print "unrecognised line element [[[\n%s\n]]]:" % line
 		print remain[:128]
@@ -105,13 +64,15 @@ def parseline(line):
 			right=right[m.end():]
 			continue
 
-# number matching
-
-		m=re.match('\s*<br>&nbsp;&nbsp;&nbsp;&nbsp;<b>&nbsp;&nbsp;&nbsp;&nbsp;<b>([1-9][0-9]*)\.</b>(&nbsp;&nbsp;&nbsp;&nbsp;)?</b>',right)
+		# number matching
+		# main section number
+		#print "*****before main section:",right[:48]
+		m=re.match('\s*<br>&nbsp;&nbsp;&nbsp;&nbsp;<b>&nbsp;&nbsp;&nbsp;&nbsp;<b>([1-9][0-9]*)(\.)?</b>(&nbsp;&nbsp;&nbsp;&nbsp;)?</b>',right)
 		if m:
+			#print "***matched section"
 			section=m.group(1)
 			path=[(section,'numeric','','')]
-			leafoptions=path2opt(path)+partoption
+			leafoptions=path2opt(path)+partoption+leftoption
 			right=right[m.end():]
 			aftersectionflag=True
 			continue
@@ -119,51 +80,25 @@ def parseline(line):
 		#print "****",right[:16]
 		m=re.match('&\#151;(\([1-9][0-9]*\))&nbsp;',right)
 		if m:
-			#item=makeitem(m.group(1))
-			
-			#n=pathsearch(path,item)
-			#if n==len(path):
-			#	path.append(item)
-			#else:
-			#	path[n]=item
-			#	path=path[:n+1]
-			
+			print "****match",right[:16]
+			pathinsert(path,m)
 			leafoptions=path2opt(path)+partoption
-			#print "****",n,path,item,m.group(1),right[:32]
 			right=right[m.end():]
 			continue
 		
 
 		m=re.match('\s*(?:<br>)*&nbsp;&nbsp;&nbsp;&nbsp;(\([1-9][0-9]*\))&nbsp;',right)
 		if m:
-			#print "***found:", m.group(1), path
 			pathinsert(path,m)
-			#item=makeitem(m.group(1))			
-			#n=pathsearch(path,item)
-			#if n==len(path):
-			#	path.append(item)
-			#else:
-			#	path[n]=item
-			#	path=path[:n+1]			
 			leafoptions=path2opt(path)+partoption
-			#print "****",n,path,item,m.group(1),right[:32]
 			right=right[m.end():]
 			continue
 
 
 		m=re.match('\s*<ul>&nbsp;(\S*)&nbsp;',right)
 		if m:
-			#print "***found:", m.group(1), path
 			pathinsert(path,m)
-			#item=makeitem(m.group(1))
-			#n=pathsearch(path,item)
-			#if n==len(path):
-			#	path.append(item)
-			#else:
-			#	path[n]=item
-			#	path=path[:n+1]
 			leafoptions=path2opt(path)+partoption
-			#print "****",path,item,m.group(1),right[:32]
 			right=right[m.end():]
 			continue
 
@@ -174,17 +109,28 @@ def parseline(line):
 			right=right[m.end():]
 			continue
 
-		m=re.match('<excision\s*/>',right)
+		m=re.match('<excision n="(\d*)"\s*/>',right)
 		if m:
 			right=right[m.end():]
 			continue			
-		else:
+
+
+		m=re.match('<quotation n="(\d)*"\s+pattype="(\w*)"\s*/>',right)
+		if m:
+			print "****found quotation"
+			right=right[m.end():]
+			output('leaf','type="quotation"',quotations[int(m.group(1))])			
+			continue
+
+		m=re.match('<',right)
+		if m:
 			print "unrecognised right line element [[[\n%s\n]]]:" % right
 			print remain[:128]
 			sys.exit()
-
-
-
+		else:
+			(t,right)=gettext(right)
+			output('leaf','type="text"',t)
+			
 
 print '<?xml version="1.0" encoding="UTF-8">'
 print '<act'
@@ -203,21 +149,63 @@ if not start:
 remain=act[start.end():]
 pos=0
 startpart=False
-part=''
 
-while re.search('<center>\s*<table>[\s\S]*</table>\s*?</center>',remain):
-	remain=re.sub('<center>\s*<table>[\s\S]*?</table>\s*</center>','<excision />',remain)
+part=''		# part subdivisions of acts, etc
+chapter=''	# chapter subdivisions of acts, etc
+division='main'	# division of the act (i.e. main part or schedule number)
 
+# Remove troublesome tables
+#
+
+tables=[]
+n=0
+tablepat='<center>\s*<table>[\s\S]*?</table>\s*?</center>'
+while re.search(tablepat,remain):
+	m=re.search(tablepat,remain)
+	tables.append(m.group())
+	#tablebody='<excision n="%s" />' % n
+	tablebody=''
+	n=n+1
+	remain=re.sub(tablepat,tablebody,remain)
+
+
+# displayed quotation processing
+
+quotations=[]
+n=0
+
+#print "****removing quotations"
+
+quotepatterns=[
+	('<table cellpadding="8">\s*<tr>\s*<td width="10%"(?: valign="top")?>&nbsp;</td>\s*<td>&quot;([\s\S]*?)&quot;\s*</td>\s*(?:</tr>\s*|(?=<tr))</table>','simple'),
+	('<table cellpadding="8">\s*<tr valign="top">\s*<td width="15%">\s*<br>([\s\S]*?)&quot;</td>\s*</tr>\s*</table>','withmargin'),
+	('<table cellpadding="8">\s*<tr valign="top">\s*<td width="5%">\s*<br>\s*</td>\s*<td>\s*<br>&nbsp;&nbsp;&nbsp;&nbsp;&quot;&nbsp;&nbsp;&nbsp;&nbsp;<b>([\s\S]*?)&quot;\s*</td>\s*</tr>\s*</table>','section')]
+
+rquotepat='<table cellpadding="8">\s*<tr>\s*<td width="10%"(?: valign="top")?>&nbsp;</td>\s*<td>&quot;([\s\S]*?)&quot;\s*</td>\s*(?:</tr>\s*|(?=<tr))</table>'
+
+mquotepat='<table cellpadding="8">\s*<tr valign="top">\s*<td width="15%">\s*<br>([\s\S]*?)&quot;</td>\s*</tr>\s*</table>'
+
+
+for (quotepat,pattype) in quotepatterns:
+	while re.search(quotepat,remain):
+		m=re.search(quotepat,remain)
+		quotations.append(m.group(1))
+		quote='<quotation n="%i" pattype="%s"/>' % (n,pattype)
+		n=n+1
+		remain=re.sub(quotepat,quote,remain)
+
+
+# Main loop
 
 while pos < len(remain):
-	m=re.match('\s*<tr valign="top">',remain)
+	m=re.match('\s*<tr valign="top">([\s\S]*?)</tr>',remain)
 	if m:
-		lineend=re.search('</tr>',remain)
-		line=remain[m.end():lineend.end()]
+		lineend=m.end()
+		line=m.group(1)
 		lpos=0
 
 		#check for centered heading
-		m=re.match('\s*<td width="20%">&nbsp;</td>\s*<td>\s*(<br>)?\s*(<i>)?\s*<center>([a-zA-Z: ]*)</center>\s*(</i>)?\s*</td>',line)
+		m=re.match('\s*<td width="20%">&nbsp;</td>\s*<td>\s*(<br>)?\s*(<i>)?\s*<center>([a-zA-Z,\-: ]*)</center>\s*(</i>)?\s*</td>',line)
 		if m:
 			#print m.group(2)
 			heading=m.group(3)
@@ -239,8 +227,22 @@ while pos < len(remain):
 			if startpart:
 				output('heading', 'type="part" n="%s"' % partno)
 			parseline(line)
+		#pos=lineend.end()
+		#remain=remain[pos:]
+		remain=remain[lineend:]
+		continue
+
+	elif 	re.match('\s*</p>\s*<tr>\s*<td width="10%">&nbsp;</td>\s*</tr>',remain):
+		lineend=re.match('\s*</p>\s*<tr>\s*<td width="10%">&nbsp;</td>\s*</tr>',remain)
 		pos=lineend.end()
 		remain=remain[pos:]
+		#remain=remain[lineend:]
+		continue
+	elif	re.match('\s*<tr>\s*<td valign="top">&nbsp;</td>\s*(?=<tr)',remain):
+		lineend=re.match('\s*<tr>\s*<td valign="top">&nbsp;</td>\s*(?=<tr)',remain)
+		pos=lineend.end()
+		remain=remain[pos:]
+		#remain=remain[lineend:]
 		continue
 	else:
 		print "unrecognised line:"
