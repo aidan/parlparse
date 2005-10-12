ca5c9bcc5365108a6257b2d7977f2a92ee41cd2e
frabcus <frabcus@9cc54934-f9f6-0310-8a8d-e8e977684441>
2005-10-12 13:37:50 +0000

    Copyright message
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@276 9cc54934-f9f6-0310-8a8d-e8e977684441

--- lawparse/opsi/fulldepage.py
+++ lawparse/opsi/fulldepage.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# fulldepage.py - download HTML of acts, top and tail and concatenate
+
+# Copyright (C) 2005 Francis Davey and Julian Todd, part of lawparse
+
+# lawparse is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+# 
+# lawparse is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with lawparse; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import urllib
 import urlparse
 import re
