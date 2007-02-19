2571c2b9e9907f3d50e3ee9848dfa699c62f71db
sym <sym@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-02-19 00:01:43 +0000

    content on pages!
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@1922 9cc54934-f9f6-0310-8a8d-e8e977684441

--- unparse/web/sites/all/themes/undemocracy/page.tpl.php
+++ unparse/web/sites/all/themes/undemocracy/page.tpl.php
@@ -1,60 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language ?>" xml:lang="<?php print $language ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language ?>" lang="<?php print $language ?>">
+  <head>
+    <title><?php print $head_title ?></title>
+    <?php print $head ?>
+    <?php print $styles ?>
+    <?php print $scripts ?>
+    </head>
 
-<head>
-  <title><?php print $head_title ?></title>
-  <?php print $head ?>
-  <?php print $styles ?>
-  <?php print $scripts ?>
-  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyle Content in IE */ ?> </script>
-</head>
+ <body>
 
-<body>
+	<h1 id="logo"><a href="<?php print url() ?>"><span class="un">UN</span>DEMOCRACY<span class="com">.COM</span></a></h1>
+	<div id="map"></div>
+	<div id="left" class="mapcol"></div>
+	<div id="right" class="mapcol"></div>
+	<div id="content">
+	<?php if ($title != ""): ?>
+          <h2 class="content-title"><?php print $title ?></h2>
+        <?php endif; ?>
+        <?php if ($tabs != ""): ?>
+          <?php print $tabs ?>
+        <?php endif; ?>
+       
+        <?php if ($mission != ""): ?>
+          <p id="mission"><?php print $mission ?></p>
+        <?php endif; ?>
+       
+        <?php if ($help != ""): ?>
+          <p id="help"><?php print $help ?></p>
+        <?php endif; ?>
+       
+        <?php if ($messages != ""): ?>
+          <div id="message"><?php print $messages ?></div>
+        <?php endif; ?>
 
-<table border="0" cellpadding="0" cellspacing="0" id="header">
-  <tr>
-    <td id="logo">
-      <?php if ($logo) { ?><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" /></a><?php } ?>
-      <?php if ($site_name) { ?><h1 class='site-name'><a href="<?php print $base_path ?>" title="<?php print t('Home') ?>"><?php print $site_name ?></a></h1><?php } ?>
-      <?php if ($site_slogan) { ?><div class='site-slogan'><?php print $site_slogan ?></div><?php } ?>
-    </td>
-    <td id="menu">
-      <?php if (isset($secondary_links)) { ?><?php print theme('links', $secondary_links, array('class' =>'links', 'id' => 'subnavlist')) ?><?php } ?>
-      <?php if (isset($primary_links)) { ?><?php print theme('links', $primary_links, array('class' =>'links', 'id' => 'navlist')) ?><?php } ?>
-      <?php print $search_box ?>
-    </td>
-  </tr>
-  <tr>
-    <td colspan="2"><div><?php print $header ?></div></td>
-  </tr>
-</table>
+        <!-- start main content -->
+        <?php print($content) ?>
+        <!-- end main content -->
+	
+	</div>
+	</body>
 
-<table border="0" cellpadding="0" cellspacing="0" id="content">
-  <tr>
-    <?php if ($sidebar_left) { ?><td id="sidebar-left">
-      <?php print $sidebar_left ?>
-    </td><?php } ?>
-    <td valign="top">
-      <?php if ($mission) { ?><div id="mission"><?php print $mission ?></div><?php } ?>
-      <div id="main">
-        <?php print $breadcrumb ?>
-        <h1 class="title"><?php print $title ?></h1>
-        <div class="tabs"><?php print $tabs ?></div>
-        <?php print $help ?>
-        <?php print $messages ?>
-        <?php print $content; ?>
-        <?php print $feed_icons; ?>
-      </div>
-    </td>
-    <?php if ($sidebar_right) { ?><td id="sidebar-right">
-      <?php print $sidebar_right ?>
-    </td><?php } ?>
-  </tr>
-</table>
-
-<div id="footer">
-  <?php print $footer_message ?>
-</div>
-<?php print $closure ?>
-</body>
 </html>
