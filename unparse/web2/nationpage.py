997acb522b3a1ccf3202f4fed427f9df030dca77
goatchurch <goatchurch@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-06-12 21:46:06 +0000

    start of nations
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@2435 9cc54934-f9f6-0310-8a8d-e8e977684441

--- unparse/web2/nationpage.py
+++ unparse/web2/nationpage.py
@@ -9,6 +9,40 @@ from basicbits import EncodeHref
 from xapsearch import XapLookup
 from indexrecords import LoadSecRecords, LoadAgendaNames
 
+def WriteSpeechInstances(snation, person):
+    print '<h3>Subselection of speeches by %s</h3>' % person
+
+    recs = XapLookup("nation:%s name:%s" % (snation, person))
+    if not recs:
+        print '<p>No results found</p>'
+        return
+
+    print '<ul>'
+    for rec in recs:
+        srec = rec.split("|")
+        gidspeech = srec[0]
+        docid = srec[1]
+        gidsubhead = srec[4]
+        print '<li><a href="%s">%s</a></li>' % (EncodeHref({"pagefunc":"meeting", "docid":docid, "gid":gidspeech}), docid)
+    print '</ul>'
+
+
+def WriteMinorityVotes(snation):
+    print '<h3>Minority votes</h3>'
+
+    recs = XapLookup("vote:%s-minority" % snation)
+    if not recs:
+        print '<p>No results found</p>'
+        return
+
+    print '<ul>'
+    for rec in recs:
+        srec = rec.split("|")
+        gidspeech = srec[0]
+        docid = srec[1]
+        gidsubhead = srec[4]
+        print '<li><a href="%s">%s</a></li>' % (EncodeHref({"pagefunc":"meeting", "docid":docid, "gid":gidspeech}), docid)
+    print '</ul>'
 
 
 # not written
@@ -17,7 +51,11 @@ def WriteIndexStuffNation(nation, person):
     flaghref = EncodeHref({"pagefunc":"flagpng", "width":100, "flagnation":nation})
     print '<h1>%s</h1>' % nation
     print '<img class="nationpageflag" src="%s">' % flaghref
+    snation = re.sub("\s", "", nation.lower())
+
     if person:
-        print '<h3>Subselection of speeches by %s</3>' % person
+        WriteSpeechInstances(snation, person)
+    else:
+        WriteMinorityVotes(snation)
 
 
