0cfe622b6258ca97b9eddf4fcb20b2cf604dd080
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-06-17 10:18:37 +0000

    Change default size
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@2519 9cc54934-f9f6-0310-8a8d-e8e977684441

--- unparse/scraper/preview.rb
+++ unparse/scraper/preview.rb
@@ -7,8 +7,8 @@ include Magick
 
 (infile, outfile, width, height) = ARGV
 
-width ||= 400
-height ||= 300
+width ||= 300
+height ||= 700
 
 width = width.to_i
 height = height.to_i
@@ -17,18 +17,19 @@ pdf = ImageList.new(infile)
 preview = Image.new(width,height) {self.background_color = "#ddffff"}
 
 cover = pdf.shift
-cover.resize_to_fit!(width * 2, height * 2)
+cover.resize_to_fit!(width * 1.7, height * 1.7) # zoom
 cover.border!(1, 1, 'black')
 cover.background_color = 'none'
 shadow = cover.dup.color_reset!("#88aaaa")
 shadow.background_color = 'none'
-cover.rotate!(-10)
+cover.rotate!(-10) # angle of rotation
 shadow.rotate!(-10)
-cover_left = (width - cover.columns) * 0.5
-cover_top = (height - cover.rows) * 0.6
+cover_left = (width - cover.columns) * 0.18 # 0 is left of page visible, 1 is right visible
+cover_top = (height - cover.rows) * 0.1 # 0 is top of page visible, 1 is bottom visible
 preview.composite! shadow, cover_left + width * 0.03, cover_top + width * 0.03, OverCompositeOp
 preview.composite! cover, cover_left, cover_top, OverCompositeOp
 
+if false
 (pdf.length - 1).downto(0) do |i|
 	offset = 10 + (i*20);
 	next if offset > width
@@ -41,6 +42,7 @@ preview.composite! cover, cover_left, cover_top, OverCompositeOp
 	page.background_color = 'none'
 	page.rotate!(20)
 	preview.composite! page, offset, height * 0.5, OverCompositeOp
+end
 end
 preview.write(outfile)
 
